cfea432530ef771ec6dfc43666ff7d28ba6fe1b4
Phuwadech <2010511101007@live4.utcc.ac.th>
2022-04-27 23:03:49 +0700

    Event2

--- validate.js
+++ validate.js
@@ -33,3 +33,18 @@ function validateForm(){
 		}
 	}
 }
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+	  alert("Passwords Don't Match");
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+
